22f51ce74b953555b7c9ad370a54ffcd71d9fc23
Maxime <maxdevvynter@gmail.com>
2024-12-17 11:16:12 +0100

    fix footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -8,10 +8,10 @@ const Footer = () => {
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-around items-center md:items-start">
           <div className="mb-4 md:mb-0">
-            <Link href="/" className="text-2xl font-bold" aria-label="Retour à l'accueil">
+            <Link href="/" className="text-2xl font-bold" aria-label="Return to homepage">
               <Image
                 src="/images/logo.png"
-                alt="Logo du Club de Volleyball"
+                alt="Volleyball Club Logo"
                 width={50}
                 height={50}
                 priority
@@ -19,28 +19,28 @@ const Footer = () => {
             </Link>
           </div>
           <address className="mb-4 md:mb-0 md:pl-14 text-center not-italic">
-            <h3 className="font-semibold mb-2">Contactez-nous</h3>
-            <p className="text-sm">Adresse : Trollesundsvägen 47, 124 34 Bandhagen</p>
-            <p className="text-sm">Téléphone : <a href="tel:+460707356835" className="hover:underline">(+46) 0707356835</a></p>
-            <p className="text-sm">Email : <a href="mailto:volleyboll@ragsvedsif.org" className="text-secondary hover:underline">volleyboll@ragsvedsif.org</a></p>
+            <h3 className="font-semibold mb-2">Contact Us</h3>
+            <p className="text-sm">Address: Trollesundsvägen 47, 124 34 Bandhagen</p>
+            <p className="text-sm">Phone: <a href="tel:+460707356835" className="hover:underline">(+46) 0707356835</a></p>
+            <p className="text-sm">Email: <a href="mailto:volleyboll@ragsvedsif.org" className="text-secondary hover:underline">volleyboll@ragsvedsif.org</a></p>
           </address>
           <div className="text-center">
-            <h3 className="font-semibold mb-2">Suivez-nous</h3>
+            <h3 className="font-semibold mb-2">Follow Us</h3>
             <div className="flex justify-center md:justify-end space-x-4">
-              <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" aria-label="Visitez notre page Facebook">
+              <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" aria-label="Visit our Facebook page">
                 <FaFacebook className="text-white w-6 h-6 hover:text-blue-600" aria-hidden="true" />
               </a>
-              <a href="https://www.instagram.com/ragsvedsif_volleybollherr?igsh=NDV4Z2prMWx3cGkw" target="_blank" rel="noopener noreferrer" aria-label="Suivez-nous sur Instagram">
+              <a href="https://www.instagram.com/ragsvedsif_volleybollherr?igsh=NDV4Z2prMWx3cGkw" target="_blank" rel="noopener noreferrer" aria-label="Follow us on Instagram">
                 <FaInstagram className="text-white w-6 h-6 hover:text-pink-500" aria-hidden="true" />
               </a>
-              <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" aria-label="Suivez-nous sur Twitter">
+              <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" aria-label="Follow us on Twitter">
                 <FaTwitter className="text-white w-6 h-6 hover:text-blue-400" aria-hidden="true" />
               </a>
             </div>
           </div>
         </div>
         <hr className="my-4 border-secondary" />
-        <p className="text-center text-sm">&copy; {new Date().getFullYear()}, Tous droits réservés.</p>
+        <p className="text-center text-sm">&copy; {new Date().getFullYear()}, All rights reserved.</p>
       </div>
     </footer>
   );
