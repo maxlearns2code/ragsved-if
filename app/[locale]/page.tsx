daebd11cbd63a3eca438ad6146bee0fa8ca41bff
Maxime <maxdevvynter@gmail.com>
2025-03-25 22:23:04 +0100

    feat: add a status section to the main page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "../components/Hero";
 import Sponsor from "../components/Sponsor";
+import Status from "../components/Status";
 
 export default function Home() {
   return (
     <div className="text-4xl">
       <Hero />
-      <Sponsor />     
+      <Status />
+      <Sponsor />
     </div>
   );
 }
---
+++ app/components/Status.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useTranslations } from "next-intl";
+
+const Sponsor = () => {
+  const t = useTranslations("Status");
+  return (
+    <section className="pt-12">
+      <div className="container mx-auto px-4">
+        <h2 className="text-3xl font-bold text-center mb-8">{t("title")}</h2>
+        <p className="text-base sm:text-lg md:text-xl lg:text-2xl text-center mb-8">
+          {t("description")}
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Sponsor;
